5a040c13651f36f902e5f9fdae64cf04e8f2f94c
g14wxz <hdcj@protonmail.com>
2024-09-12 07:54:57 -0600

    feat(amazingstore): add missing code and pgsql support

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +20,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/login', function (Request $request) {
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+    // Create a token for the user
+    $token = $user->createToken('api-token')->plainTextToken;
+
+    return response()->json(['token' => $token], 200);
+})->name('login');
 
 Route::resource('categories', App\Http\Controllers\API\CategoryAPIController::class);
 
